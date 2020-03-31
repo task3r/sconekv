21865a2f8f1b71254e3569f148aeeed0e00d2270
João Gonçalves <joaogoncalves8@hotmail.com>
2020-03-31 12:19:28 +0100

    simplest of client-server using zeromq

---
+++ server/src/main/java/pt/ulisboa/tecnico/sconekv/server/Application.java
@@ -0,0 +1,25 @@
+package pt.ulisboa.tecnico.sconekv.server;
+
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+public class Application {
+    public static void main(String[] args) throws Exception {
+        try (ZContext context = new ZContext()) {
+            // Socket to talk to clients
+            ZMQ.Socket socket = context.createSocket(SocketType.REP);
+            socket.bind("tcp://*:5555");
+
+            while (!Thread.currentThread().isInterrupted()) {
+                byte[] reply = socket.recv(0);
+                System.out.println("Received " + ": [" + new String(reply, ZMQ.CHARSET) + "]");
+
+                String response = "world";
+                socket.send(response.getBytes(ZMQ.CHARSET), 0);
+
+                Thread.sleep(1000); //  Do some 'work'
+            }
+        }
+    }
+}
