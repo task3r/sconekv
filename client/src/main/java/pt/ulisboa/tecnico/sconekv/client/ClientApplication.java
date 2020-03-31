5c8c5285f5cd5a661a1231108c3176ef74af57ab
João Gonçalves <joaogoncalves8@hotmail.com>
2020-03-31 12:54:24 +0100

    added logging

--- client/src/main/java/pt/ulisboa/tecnico/sconekv/client/ClientApplication.java
+++ client/src/main/java/pt/ulisboa/tecnico/sconekv/client/ClientApplication.java
@@ -1,25 +1,29 @@
 package pt.ulisboa.tecnico.sconekv.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
-public class Application {
+public class ClientApplication {
+    private static final Logger logger = LoggerFactory.getLogger(ClientApplication.class);
     public static void main(String[] args) {
+        logger.info("Client created.");
         try (ZContext context = new ZContext()) {
             //  Socket to talk to server
-            System.out.println("Connecting to hello world server");
+            logger.info("Connecting to hello world server");
 
             ZMQ.Socket socket = context.createSocket(SocketType.REQ);
             socket.connect("tcp://localhost:5555");
 
             for (int requestNbr = 0; requestNbr != 10; requestNbr++) {
                 String request = "Hello";
-                System.out.println("Sending Hello " + requestNbr);
+                logger.info("Sending Hello {}", requestNbr);
                 socket.send(request.getBytes(ZMQ.CHARSET), 0);
 
-                byte[] reply = socket.recv(0);
-                System.out.println("Received " + new String(reply, ZMQ.CHARSET) + " " + requestNbr);
+                String reply = new String(socket.recv(0), ZMQ.CHARSET);
+                logger.info("Received {} {}.", reply, requestNbr);
             }
         }
     }
--- server/src/main/java/pt/ulisboa/tecnico/sconekv/server/ServerApplication.java
+++ server/src/main/java/pt/ulisboa/tecnico/sconekv/server/ServerApplication.java
@@ -1,19 +1,23 @@
 package pt.ulisboa.tecnico.sconekv.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
-public class Application {
+public class ServerApplication {
+    private static final Logger logger = LoggerFactory.getLogger(ServerApplication.class);
     public static void main(String[] args) throws Exception {
+        logger.info("Server created.");
         try (ZContext context = new ZContext()) {
             // Socket to talk to clients
             ZMQ.Socket socket = context.createSocket(SocketType.REP);
             socket.bind("tcp://*:5555");
 
             while (!Thread.currentThread().isInterrupted()) {
-                byte[] reply = socket.recv(0);
-                System.out.println("Received " + ": [" + new String(reply, ZMQ.CHARSET) + "]");
+                String reply = new String(socket.recv(0), ZMQ.CHARSET);
+                logger.info("Received: [{}]", reply);
 
                 String response = "world";
                 socket.send(response.getBytes(ZMQ.CHARSET), 0);
