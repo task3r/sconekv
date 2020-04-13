41cbe1e8d87e11709ad9e4265c1d13eb68e6cb46
João Gonçalves <joaogoncalves8@hotmail.com>
2020-04-13 19:31:20 +0100

    serialization capnproto to and from bytes

---
+++ common/src/main/java/pt/ulisboa/tecnico/sconekv/common/utils/SerializationUtils.java
@@ -0,0 +1,28 @@
+package pt.ulisboa.tecnico.sconekv.common.utils;
+
+import org.capnproto.MessageBuilder;
+import org.capnproto.MessageReader;
+import org.capnproto.SerializePacked;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.channels.Channels;
+
+public class SerializationUtils {
+    private static final Logger logger = LoggerFactory.getLogger(SerializationUtils.class);
+
+    public static byte[] getBytesFromMessage(MessageBuilder message) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        SerializePacked.writeToUnbuffered(Channels.newChannel(baos), message);
+        return baos.toByteArray();
+    }
+
+    public static MessageReader getMessageFromBytes(byte[] message) throws IOException {
+        ByteArrayInputStream bais = new ByteArrayInputStream(message);
+        return SerializePacked.readFromUnbuffered(Channels.newChannel(bais));
+    }
+
+}
