ebbe8058864729bb3994e8a0af22582421882945
João Gonçalves <joaogoncalves8@hotmail.com>
2020-07-08 13:04:28 +0100

    added threads as env variable in ycsb container

--- ycsb/scripts/load.sh
+++ ycsb/scripts/load.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp sconekv-binding-1.0-SNAPSHOT-jar-with-dependencies.jar site.ycsb.Client -load -db pt.ulisboa.tecnico.sconekv.ycsb.SconeKVClient -P $WORKLOAD
+java -cp sconekv-binding-1.0-SNAPSHOT-jar-with-dependencies.jar site.ycsb.Client -load -db pt.ulisboa.tecnico.sconekv.ycsb.SconeKVClient -P $WORKLOAD -threads $THREADS
