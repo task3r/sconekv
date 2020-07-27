c1f32cb955f5692c569644038db97a0c773512fa
João Gonçalves <joaogoncalves8@hotmail.com>
2020-07-27 15:47:35 +0100

    added writeall workloads

--- ycsb/scripts/benchmark.sh
+++ ycsb/scripts/benchmark.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp sconekv-binding-1.0-SNAPSHOT-jar-with-dependencies.jar site.ycsb.Client -t -db pt.ulisboa.tecnico.sconekv.ycsb.SconeKVClient -P $WORKLOAD -threads $THREADS -s -p scone.tx_size=$BENCH_TX_SIZE
+java -cp sconekv-binding-1.0-SNAPSHOT-jar-with-dependencies.jar site.ycsb.Client -t -db pt.ulisboa.tecnico.sconekv.ycsb.SconeKVClient -P $WORKLOAD -threads $THREADS -s -p scone.tx_size=$BENCH_TX_SIZE |& tee "out/"$WORKLOAD-$THREADS-$BENCH_TX_SIZE-`date +%Y%m%d-%H%M%S`.log
