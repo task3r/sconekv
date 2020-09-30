6203cfd846dcba7bbc7cd1e202b2b9fa3f07ac4b
João Gonçalves <joaogoncalves8@hotmail.com>
2020-09-30 12:24:12 +0100

    fix benchmark to use zipfian distribution

--- ycsb/scripts/benchmark.sh
+++ ycsb/scripts/benchmark.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp sconekv-binding-1.0-SNAPSHOT-jar-with-dependencies.jar site.ycsb.Client -t -db pt.ulisboa.tecnico.sconekv.ycsb.SconeKVClient -P $WORKLOAD -threads $THREADS -s -p scone.tx_size=$BENCH_TX_SIZE -p operationcount=10000000 -p maxexecutiontime=300 -p requestdistribution=uniform |& tee "out/sconekv/"$WORKLOAD-$THREADS-$BENCH_TX_SIZE-`date +%Y%m%d-%H%M%S`.log
+java -cp sconekv-binding-1.0-SNAPSHOT-jar-with-dependencies.jar site.ycsb.Client -t -db pt.ulisboa.tecnico.sconekv.ycsb.SconeKVClient -P $WORKLOAD -threads $THREADS -s -p scone.tx_size=$BENCH_TX_SIZE -p operationcount=10000000 -p maxexecutiontime=300 |& tee "out/sconekv/"$WORKLOAD-$THREADS-$BENCH_TX_SIZE-`date +%Y%m%d-%H%M%S`.log
