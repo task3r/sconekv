77e953766c60bcc4eda336e685f8ad80268a3ace
João Gonçalves <joaogoncalves8@hotmail.com>
2021-03-24 18:22:19 +0000

    update version in dockerfile, add 4 node stack

--- server/scripts/deploy-nodes.sh
+++ server/scripts/deploy-nodes.sh
@@ -26,19 +26,19 @@ tell application "iTerm"
 
     # Exec commands
     tell first session of current tab of current window
-        write text "docker run --rm -it --network sconenet task3r/sconekv-node"
+        write text "docker run --rm -it --network sconenet -v ~/dev/sconekv/deployment/configs/4-nodes.properties:/usr/src/sconekv/config.properties task3r/sconekv-node"
     end tell
     delay 1
     tell second session of current tab of current window
-        write text "docker run --rm -it --network sconenet task3r/sconekv-node"
+        write text "docker run --rm -it --network sconenet -v ~/dev/sconekv/deployment/configs/4-nodes.properties:/usr/src/sconekv/config.properties task3r/sconekv-node"
     end tell
     delay 1
     tell third session of current tab of current window
-        write text "docker run --rm -it --network sconenet task3r/sconekv-node"
+        write text "docker run --rm -it --network sconenet -v ~/dev/sconekv/deployment/configs/4-nodes.properties:/usr/src/sconekv/config.properties task3r/sconekv-node"
     end tell
     delay 1
     tell fourth session of current tab of current window
-        write text "docker run --rm -it --network sconenet task3r/sconekv-node"
+        write text "docker run --rm -it --network sconenet -v ~/dev/sconekv/deployment/configs/4-nodes.properties:/usr/src/sconekv/config.properties task3r/sconekv-node"
     end tell
 end tell
 '
--- server/scripts/start-container.sh
+++ server/scripts/start-container.sh
@@ -1,2 +1,2 @@
 echo 'start sconekv container'
-java "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005" -jar server-1.0-SNAPSHOT-jar-with-dependencies.jar
+java "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005" -jar server-1.0-jar-with-dependencies.jar
